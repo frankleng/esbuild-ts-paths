f054af9acabd9d9508b039ea7bf804700ba84a73
Frank Leng <email@frankleng.me>
2022-01-25 03:01:44 -0800

    init
    add doc
    
    fix path not found
    
    remove yarn 3 pnp
    
    bump 1.0.3

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # esbuild-ts-paths
 Transform TS path alias to absolute paths for esbuild
+
+```javascript
+const tsPaths = require("esbuild-ts-paths") 
+esbuild.build({
+    //...
+    plugins:[
+        tsPaths(
+            "./path/to/tsconfig.json" // optional, defaults to ./tsconfig.json
+        )
+    ]
+})
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const { sync: glob } = require("fast-glob");
+const path = require("path");
+
+module.exports = (tsconfigPath = './tsconfig.json') => {
+  const { compilerOptions } = require(path.resolve(process.cwd(), tsconfigPath));
+  const pathKeys = Object.keys(compilerOptions.paths);
+  const re = new RegExp(`^(${pathKeys.join("|")})`);
+  return {
+    name: "esbuild-ts-paths",
+    setup(build) {
+      build.onResolve({ filter: re }, (args) => {
+        const pathKey = pathKeys.find((pkey) => new RegExp(`^${pkey}`).test(args.path));
+        const [pathDir] = pathKey.split("*");
+        const file = args.path.replace(pathDir, "");
+        for (const dir of compilerOptions.paths[pathKey]) {
+          const [matchedFile] = glob(`${path.resolve(process.cwd(), dir).replace("*", file)}.*`);
+          if (matchedFile) {
+            return { path: matchedFile };
+          }
+        }
+        return { path: args.path };
+      });
+    },
+  };
+};
