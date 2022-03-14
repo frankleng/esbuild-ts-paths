527121141d0f074dc7af0dd9096cc07309fc790b
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2022-03-14 13:56:17 +0000

    style: prettify files

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const { sync: glob } = require("fast-glob");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 
-function stripJsonComments (data) {
-  return data.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m, g) => g ? "" : m);
+function stripJsonComments(data) {
+  return data.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m, g) => (g ? "" : m));
 }
 
-module.exports = (relativeTsconfigPath = './tsconfig.json') => {
+module.exports = (relativeTsconfigPath = "./tsconfig.json") => {
   const absTsconfigPath = path.resolve(process.cwd(), relativeTsconfigPath);
-  let tsconfigData = fs.readFileSync(absTsconfigPath, 'utf8');
+  let tsconfigData = fs.readFileSync(absTsconfigPath, "utf8");
   tsconfigData = stripJsonComments(tsconfigData);
-  const {compilerOptions} = JSON.parse(tsconfigData);
+  const { compilerOptions } = JSON.parse(tsconfigData);
 
   const pathKeys = Object.keys(compilerOptions.paths);
   const re = new RegExp(`^(${pathKeys.join("|")})`);
@@ -22,11 +22,11 @@ module.exports = (relativeTsconfigPath = './tsconfig.json') => {
         const [pathDir] = pathKey.split("*");
         const file = args.path.replace(pathDir, "");
         for (const dir of compilerOptions.paths[pathKey]) {
-          const fileDir = path.resolve(process.cwd(), dir).replace("*", file)
+          const fileDir = path.resolve(process.cwd(), dir).replace("*", file);
           let [matchedFile] = glob(`${fileDir}.*`);
           if (!matchedFile) {
             const [matchIndexFile] = glob(`${fileDir}/index.*`);
-            matchedFile = matchIndexFile
+            matchedFile = matchIndexFile;
           }
           if (matchedFile) {
             return { path: matchedFile };
